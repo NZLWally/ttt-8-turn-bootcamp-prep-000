bf5e8e00d2597708580019391bbab015c28da043
James Wallace <4538317+nzlwally@users.noreply.github.com>
2020-06-15 03:09:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,11 +10,11 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  index.between?(0, 8) # && !position_taken?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 def position_taken?(board, index)
-  board[index] == " " || board[index] == "" || board[index] == nil
+  board[index] != " " && board[index] != "" && board[index] != nil
 end
 
 def move(board, index, token = 'X')
