ed8cee0915ebaca1c97ec7b0cffeb168809be946
James Wallace <4538317+nzlwally@users.noreply.github.com>
2020-06-15 02:51:19 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def display_board(board)
+  # print state of board
+end
+
+def valid_move?(board, index)
+  index.between(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+
+end
+
+def move(board, index, token = 'X')
+  
+end
+
+def input_to_index(input)
+  
+end
