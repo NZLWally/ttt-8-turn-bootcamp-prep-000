96d6094c5933b0cb4a6b3fc198b011c4fa67591d
James Wallace <4538317+nzlwally@users.noreply.github.com>
2020-06-15 03:38:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index, 'X')
-    # display(board)
+    display_board(board)
   else
     turn(board)
   end
